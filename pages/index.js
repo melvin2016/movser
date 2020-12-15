03eff8bb37609453c6e3b28949cca0f0f232aeb3
MELVIN GEORGE <melvingeorge10@gmail.com>
2020-12-15 17:21:41 +0530

    removed github repo link

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,54 @@
-import React from 'react';
-import Layout from '../components/Layout';
-const Index = (props)=>{
-    return(
-        <Layout>
-            <div className="container">
-                <div className="row">
-                    <div className="col s12">
-                        <h1 className="center-align">MOVSER!</h1>
-                        <div className="divider"/>
-                    </div>
+import React from "react";
+import Layout from "../components/Layout";
+const Index = (props) => {
+  return (
+    <Layout>
+      <div className="container">
+        <div className="row">
+          <div className="col s12">
+            <h1 className="center-align">MOVSER!</h1>
+            <div className="divider" />
+          </div>
+        </div>
+
+        <div className="row section">
+          <div className="col m12 s12 l6">
+            <div className="card hoverable teal lighten-1 z-depth-4">
+              <div className="card-content">
+                <div className="card-title">
+                  <span className="white-text">NOTE</span>
                 </div>
-                
-                <div className="row section">
-                    <div className="col m12 s12 l6">
-                        <div className="card hoverable teal lighten-1 z-depth-4">
-                            <div className="card-content">
-                                <div className="card-title">
-                                    <span className="white-text">NOTE</span>
-                                </div>
-                                <p className="white-text">
-                                    MOVSER is a Web-App for retrieving Information on Movies/Series .<br/>
-                                    The Technology Behind this is Next.js(React.js) , Nodejs, Expressjs .<br/>
-                                    Powered By TMDB.
-                                </p>
-                            </div>
-                            <div className="card-action">
-                            <a href="https://github.com/melvin2016/movserNext">GitHub Repo</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col m12 s12 l6 valign-wrapper">
-                        <img className="responsive-img center" alt="Powered By TMDB LOGO"  src="/static/tmdb.png"></img>
-                    </div>
-                </div>
-                <style jsx>{`
-                    .center{
-                        display: block;
-                        margin: 0 auto;
-                    }
-                    .movser{
-                        height:240px;
-                        width:240px;
-                    }
-            
-                `}</style>
+                <p className="white-text">
+                  MOVSER is a Web-App for retrieving Information on
+                  Movies/Series .<br />
+                  The Technology Behind this is Next.js(React.js) , Nodejs,
+                  Expressjs .<br />
+                  Powered By TMDB.
+                </p>
+              </div>
+              <div className="card-action"></div>
             </div>
-        </Layout>
-    );
-}
+          </div>
+          <div className="col m12 s12 l6 valign-wrapper">
+            <img
+              className="responsive-img center"
+              alt="Powered By TMDB LOGO"
+              src="/static/tmdb.png"
+            ></img>
+          </div>
+        </div>
+        <style jsx>{`
+          .center {
+            display: block;
+            margin: 0 auto;
+          }
+          .movser {
+            height: 240px;
+            width: 240px;
+          }
+        `}</style>
+      </div>
+    </Layout>
+  );
+};
 export default Index;
